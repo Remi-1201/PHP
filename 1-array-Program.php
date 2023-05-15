23232622c00e4cae6c43a5d300fd848cf601388f
remi <remi.nkjm@gmail.com>
2023-05-15 20:35:48 +0900

    1-array-Program

---
+++ 1-array-Program.php
@@ -0,0 +1,31 @@
+<?php
+
+1.“1から5で評価を入力してください。終了する場合は「6」を入力してください”と出力する
+2.readline で入力された評価ポイント受け取る
+3.数字以外を入力されたら”数字を入力してください”と出力して入力モードに、6が入力されたら”終了します”と出力してプログラムを終了する
+4.readline で入力されたコメントを受け取る
+5.過去の評価ポイントとコメントを”ポイント：○ コメント：○○○”と言う形で全件出力する（プログラムが終了した時点でデータは消えます）
+6.1に戻る
+
+$posts = [];
+while (true) {
+    $point = intval(readline('1から5の数字で評価を入力してください。終了する場合は「6」を入力してください' . PHP_EOL));
+    if ($point === 6) {
+        echo '終了します' . PHP_EOL;
+        break;
+    } elseif ($point !== 0) {
+        $comment = readline('コメントを入力してください' . PHP_EOL);
+        $post = [
+            'point'  => $point,
+            'comment'=> $comment,
+        ];
+        array_push($posts, $post);
+        foreach ($posts as $post) {
+            $point = $post['point'];
+            $comment = $post['comment'];
+            echo "ポイント: $point コメント: $comment" . PHP_EOL;
+        }
+    } else {
+        echo '数字を入力してください' . PHP_EOL;
+    }
+}
