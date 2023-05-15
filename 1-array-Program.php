47a0dcb50cb1b3871dd5091746a9844c2092f8c9
remi <remi.nkjm@gmail.com>
2023-05-15 20:54:51 +0900

    1-array-Program comment update

--- 1-array-Program.php
+++ 1-array-Program.php
@@ -11,11 +11,14 @@
 $posts = [];
 
 while (true) {
+    // readline で入力された評価ポイント受け取る
     $point = intval(readline('1から5の数字で評価を入力してください。終了する場合は「6」を入力してください' . PHP_EOL));
+    // 6が入力されたら
     if ($point === 6) {
         echo '終了します' . PHP_EOL;
         break;
     } elseif ($point !== 0) {
+        // readline で入力されたコメントを受け取る
         $comment = readline('コメントを入力してください' . PHP_EOL);
         //入力された値が入った配列
         $post = [
@@ -29,9 +32,11 @@ while (true) {
         foreach ($posts as $post) {
             $point = $post['point'];
             $comment = $post['comment'];
+            // 過去の評価ポイントとコメントを全件出力
             echo "ポイント: $point コメント: $comment" . PHP_EOL;
         }
     } else {
+        // 数字以外を入力されたら
         echo '数字を入力してください' . PHP_EOL;
     }
 }
